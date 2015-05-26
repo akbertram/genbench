6beedb9b5359619700272b3133e8d1f6bb0d4a34
biolion <ieuan.clay@gmail.com>
2015-05-26 10:55:53 +0200

    updated run script to search for any loaded packages

--- run_benchmarks.R
+++ run_benchmarks.R
@@ -5,7 +5,7 @@
 ### run all benchmarks in genbench
 
 # Install required packages
-install.dependencies <- function() {
+install.dependencies <- function(cran=c(), bioc=c() {
   
   # Set a CRAN mirror to use
   options(repos=structure(c(CRAN="http://cran.rstudio.com")))
@@ -14,16 +14,7 @@ install.dependencies <- function() {
   .libPaths(new="~/R/libs")
   
   # Install CRAN packages
-  for(pkg in c(
-                # clustering
-                'stats','biclust', 's4vd', 'irlba',
-                # table processing
-                "plyr", "reshape","sqldf",
-                # utils
-                "utils", "R.utils", "XML",
-                # graph models
-                "igraph"
-               )) {
+  for(pkg in cran) {
     if(!(pkg %in% installed.packages())) {
       install.packages(pkg, dependencies = TRUE)
     }
@@ -31,15 +22,40 @@ install.dependencies <- function() {
   
   # Install bioconductor packages
   source("http://bioconductor.org/biocLite.R")
-  for(pkg in c('Biobase', 'affy', 'hgu133plus2cdf', 'limma', 'edgeR')) {
+  for(pkg in bioc) {
     if(!(pkg %in% installed.packages())) {
       biocLite(pkg, suppressUpdates = TRUE, suppressAutoUpdate = TRUE, ask = FALSE)
     }
   }
 }
 
-# install required packages if any
-install.dependencies()
+## install required packages if any
+# find any dependencies
+if (FALSE){
+  # find and report
+  for (SCRIPT in rev(dir(file.path(getwd(), "code"), 
+                         full.names = TRUE, recursive = TRUE, pattern = "\\.R$", 
+                         ignore.case = TRUE))){
+    # run benchmark script
+    cat("Packages found in ", SCRIPT,"\n")
+    lines <- readLines(SCRIPT)
+    print(lines[grepl("library", lines)])
+    print(lines[grepl("require", lines)])
+  }
+  
+}
+cran <- c(
+  # clustering
+  'stats','biclust', 's4vd', 'irlba',
+  # table processing
+  "plyr", "reshape","sqldf",
+  # utils
+  "utils", "R.utils", "XML",
+  # graph models
+  "igraph"
+)
+bioc <- c('Biobase', 'affy', 'hgu133plus2cdf', 'limma', 'edgeR')
+install.dependencies(bioc=bioc, cran=cran)
 
 # find and run all benchmark scripts
 for (SCRIPT in rev(dir(file.path(getwd(), "code"), 
@@ -47,7 +63,7 @@ for (SCRIPT in rev(dir(file.path(getwd(), "code"),
                   ignore.case = TRUE))){
   # run benchmark script
   cat(timestamp(quiet = TRUE), "Running benchmark at ", SCRIPT,"\n")
-  try({source(SCRIPT, chdir = TRUE)},silent = T) # all scripts assume working dir is same as script
+  try({source(SCRIPT, chdir = TRUE)}) # all scripts assume working dir is same as script
   
 }
 # multiruns
