8ea36514c2ebf4decf38c718caa7c095185ab38f
biolion <ieuan.clay@gmail.com>
2015-05-29 10:29:17 +0200

    started "clinical" section
    aimed at ML appraoches on clinical data

---
+++ code/clinical/esrII.R
@@ -0,0 +1,18 @@
+library(ncvreg) # source datasets from http://cran.r-project.org/web/packages/ncvreg/ncvreg.pdf
+
+
+
+### heart dataset
+# Hastie, T., Tibshirani, R., and Friedman, J. (2001). The Elements of Statistical Learning. Springer. 
+# Rousseauw, J., et al. (1983). Coronary risk factor screening in three rural communities. South African Medical Journal, 64, 430-436.
+data(heart)
+
+### prostate dataset
+# Hastie, T., Tibshirani, R., and Friedman, J. (2001). The Elements of Statistical Learning. Springer. 
+# Stamey, T., et al. (1989). Prostate specific antigen in the diagnosis and treatment of adenocarcinoma of the prostate. II. Radical prostatectomy treated patients. Journal of Urology, 16: 1076-1083.
+data(prostate)
+
+### lung dataset
+# http://CRAN.R-project.org/package=survival
+# Kalbfleisch D and Prentice RL (1980), The Statistical Analysis of Failure Time Data. Wiley, New York.
+data(Lung)
