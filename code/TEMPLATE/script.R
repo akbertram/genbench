ee0696649654fab6bf27a31ff4abcbc754c153f0
biolion <ieuan.clay@gmail.com>
2015-06-01 10:40:44 +0200

    added (working) template code

--- code/TEMPLATE/script.R
+++ code/TEMPLATE/script.R
@@ -13,12 +13,13 @@ stopifnot(file.exists(file.path("..","..", "data"))) # data path is relative
 source(file.path("..", "..","benchmark_utilities.R"))
 
 ## packages
-TEMPLATE
+# CRAN
+# Bioc
 
 ## global vars
 VERBOSE <- TRUE # print progress?
 DOWNLOAD <- FALSE # download fresh data?
-BENCHMARK <- TEMPLATE
+BENCHMARK <- "TEMPLATE"
 
 # holder for results
 RESULTS <- results(benchmark_name = BENCHMARK)
@@ -27,15 +28,37 @@ TIMES <- timings(benchmark_name = BENCHMARK)
 ### functions
 ## Utility
 
-## Timing blocks
-
+## Blocks for timing
+do.TEMPLATE <-function(plot_results=FALSE){
+  ### this funciton is a template
+  # it does some stuff
+  # it expects some stuff
+  
+  df <- data.frame(x=rnorm(10000), y=rnorm(10000))
+  df$d <- sqrt(df$x**2 + df$y**2)
+  df$col <- factor(round(df$d))
+  df <- df[ order(df$d, decreasing = TRUE), ]
+  
+  if(plot_results){
+    
+    plot(x=df$x, y=df$y, 
+         type="p", 
+         main="evil eye template", xlab="X", ylab="Y",
+         col=df$col, pch=20, 
+         )
+    
+  }
+  
+  return(head(df))
+    
+}
 
 ### reporting
 # TEMPLATE
-TIMES <- addRecord(TIMES, record_name = TEMPLATE,
+TIMES <- addRecord(TIMES, record_name = "TEMPLATE",
                    record = system.time(gcFirst = T,
-                                        RESULTS <- addRecord(RESULTS, record_name=TEMPLATE,
-                                                             record=do.TEMPATE()
+                                        RESULTS <- addRecord(RESULTS, record_name="TEMPLATE",
+                                                             record=do.TEMPLATE()
                    ))
 
 ## output results for comparison
