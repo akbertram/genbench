757872e611a4eb8afb676f938a7b81d100f45f28
Parham Solaimani <parham@bedatadriven.com>
2015-11-18 11:33:28 +0100

    merged survival R scripts, to avoid jenkins running *functions.R as standalone

--- code/survival/survival_tcga.R
+++ code/survival/survival_tcga.R
@@ -2,17 +2,17 @@
 # andre.verissimo@tecnico.ulisboa.pt
 # November 2015
 
-### set up session
+##### set up session #####
 rm(list=ls())
 # reproducibility
 set.seed(8008)
 stopifnot(file.exists(file.path("..", "..", "data"))) # data path is relative
 
-## packages
+##### loading packages #####
 library(glmnet)
 library(survival)
 
-## global vars
+##### Set global vars #####
 VERBOSE <- TRUE # print progress?
 INPUT <- "../../data/survival/tcga_ov.csv"
 
@@ -35,9 +35,57 @@ params$nlambda <- 10000 # number of diferent lambdas to be tested
 params$alpha   <- seq(0,1,0.1)
 params$nalpha  <- length(params$alpha)
 
-source("survival_functions.R")
+##### Blocks for timing #####
 
-### run functions and time them
+do.load <- function(INPUT){
+  print('Loading all necessary data...')
+  ### import data
+  # read data into data.frames
+  temp_dat <- read.csv(INPUT)
+  dat <- data.frame( temp_dat[,c(2:(dim(temp_dat)[2]))], row.names = temp_dat[,1] )
+  #
+  var_len = dim(dat)[2]
+  var_arr = 3:var_len
+  # get the variables from data
+  #  ydata is a data.frame keeps the status of the patient and time of last follow-up
+  ydata     <- cbind( time=dat$time, status=dat$status)
+  #  xdata keeps the gene expression for each patient
+  xdata     <- dat[,var_arr]
+  #
+  surv_data <- list(ydata = ydata, xdata=xdata)
+  return(surv_data)
+}
+
+do.calc <- function(surv_data){
+  print('Starting calculation...')
+  #
+  alpha_vec  <- array(0,params$nalpha)
+  #
+  xdata <- as.matrix(surv_data$xdata)
+  ydata <- surv_data$ydata
+  #
+  # create results structure, as it was not possible to determine before the first loop
+  results = list()
+  # for each alpha and lambda, determine the glmnet
+  for (mm in 1:length(params$alpha)) {
+    # set the alpha value
+    alpha_v = params$alpha[mm]
+    # get local results
+    temp_results = glmnet( xdata, ydata, family='cox', alpha=alpha_v, nlambda=params$nlambda, standardize=FALSE )
+    # save results
+    item = list()
+    item$lambda = temp_results$lambda
+    item$beta   = temp_results$beta
+    results[[mm]] <- item
+  }
+  alpha_vec  <- params$alpha
+  return( list( results=results, alphas=alpha_vec))
+}
+
+############################################################################
+################### TIMING AND REPORTING ###################################
+############################################################################
+
 ## load data and compute matrix
 TIMES <- addRecord(TIMES, record_name = "load",
                    record = system.time(gcFirst = T,
